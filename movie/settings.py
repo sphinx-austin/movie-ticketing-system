7dfee4870bcc8be1c2175c311f928d049f1c62bd
sphinx-austin <austinjuniour4@gmail.com>
2023-12-01 21:42:35 +0300

    config dirs

--- movie/settings.py
+++ movie/settings.py
@@ -119,6 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+# add
+MEDIA_URL= 'media/'
+STATICFILES_DIRS=[BASE_DIR / 'static']
+STATIC_ROOT=BASE_DIR / 'static_root'
+MEDIA_ROOT=BASE_DIR / 'media_files'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
