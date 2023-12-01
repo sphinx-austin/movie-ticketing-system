b2cf2f031f6ad122317dfc365fc49620a8a9ec59
sphinx-austin <austinjuniour4@gmail.com>
2023-12-01 21:43:33 +0300

    add WatchDay model

--- movie_ticket/models.py
+++ movie_ticket/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class WatchDay(models.Model):
+    date =  models.DateField()
+
+    def __str__(self) -> str:
+        return f"day-of-movies-{self.id}"
