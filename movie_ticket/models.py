4f6be68733367231e9e9884f23e49c0b8f16f0bd
sphinx-austin <austinjuniour4@gmail.com>
2023-12-02 22:07:10 +0300

    add Movie model

--- movie_ticket/models.py
+++ movie_ticket/models.py
@@ -6,3 +6,18 @@ class WatchDay(models.Model):
 
     def __str__(self) -> str:
         return f"day-of-movies-{self.id}"
+
+class Movie(models.Model):
+    title=models.CharField(max_length=200)
+    genre=models.CharField(max_length=200)
+    description=models.TextField()
+    duration=models.CharField(max_length=50, default="1hour 30mins")
+    release_date=models.DateField()
+    rating=models.IntegerField(default=0)
+    casts=models.CharField(max_length=500)
+    view_price=models.DecimalField(max_digits=7, decimal_places=2)
+    movies_art=models.ImageField(upload_to="thumbnail")
+    view_date=models.ManyToManyField(WatchDay)
+
+    def __str__(self) -> str:
+        return self.title
